d6d6c201d964796eea438c3925190dd195fd2595
danilolopesabreu <danilo.bcc@gmail.com>
2019-04-01 21:14:41 -0300

    login e logout

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,15 +1,31 @@
+import { UserService } from './../user/user.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { TokenService } from './token/token.service';
+
 const API_URL = 'http://localhost:3000';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private userService: UserService) { }
   
   authenticate(userName: string, password: string) {
 
-    return this.http.post(API_URL + '/user/login', { userName, password } )
+    return this.http.post(
+      API_URL + '/user/login',
+      { userName, password },
+      { observe: 'response' }//dar acesso ao response para res.headers.get poder acessar header
+    )
+    .pipe(tap(res => {
+      const authToken = res.headers.get('x-access-token');
+      this.userService.setToken(authToken);
+      console.log(`User ${userName} authenticated with token ${authToken}`);
+  }));
   }
 }
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,30 @@
+import { UserService } from './../user/user.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  user$: Observable<User>;
+
+  constructor(
+    private userService:UserService,
+    private router:Router) { 
+    this.user$ = this.userService.getUser();
+  }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
+
+}
